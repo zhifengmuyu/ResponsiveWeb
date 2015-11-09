d77404cc6c3444592251c163717dd53d9265fdc1
zifeng0 <zifeng0@sina.com>
2015-11-09 23:33:51 +0800

    1. breadcrumbs added 2. layout the page title for each

--- exam/exam03/Assignment/assignment.js
+++ exam/exam03/Assignment/assignment.js
@@ -1,19 +1,39 @@
 // put your javascript code here
 var animal_template;
 $(document).ready(function(){
+	//add breadcrumb
+	$(".breadcrumb").html("<li><a href='#'>Animal</a></li>")
+	
+	//instantiate the template
 	var source = $("#animal_classification").html();
 	animal_template = Handlebars.compile(source);
 	
 	var content = animal_template(animals);
 	$("#content").html(content);
 	
-	
+	//after above template is instantiated, js can be added
 	$(".zoo_class_image").click(function(){
 		var source = $("#class_of_animal").html();
 		animal_template = Handlebars.compile(source);
 
 		var content = animal_template(animals.class[parseInt($(this).attr("id"))]);
+		var class_index = parseInt($(this).attr("id"))
 		$("#content").html(content);
+		
+		//after second level of template instantiated, js added 
+		$(".zoo_animal_image").click(function(){
+			var source = $("#individual_animal").html();
+			animal_template = Handlebars.compile(source);
+
+			var content = animal_template(animals.class[class_index].animals[parseInt($(this).attr("id"))]);
+			$("#content").html(content);
+			
+			$("#animal_class").text(animals.class[class_index].name);
+		})
+		
+		
 	})
+	
+	
 }
 );
