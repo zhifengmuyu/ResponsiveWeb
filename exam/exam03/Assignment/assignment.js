e153f2b8e5f205e222adad430728e122b02baedf
zifeng0 <zifeng0@sina.com>
2015-11-11 23:26:18 +0800

    add banner.png simplify code

--- exam/exam03/Assignment/assignment.js
+++ exam/exam03/Assignment/assignment.js
@@ -6,6 +6,10 @@ function loadClassAnimal(class_index){
 		var source = $("#class_of_animal").html();
 		animal_template = Handlebars.compile(source);
 
+		//check whether function is called from breadcrumbs or from clicking the image.
+		if(typeof(class_index) != "number")//if it is called from image, class_index need to be extracted from id attribute
+			class_index = parseInt($(this).attr("id"));
+			
 		var content = animal_template(animals.class[class_index]);
 		$("#content").html(content);
 		
@@ -40,36 +44,8 @@ function loadZooAnimals(){
 	$("#content").html(content);
 	
 	//after above template is instantiated, js can be added
-	$(".zoo_class_image").click(function(){
-		var source = $("#class_of_animal").html();
-		animal_template = Handlebars.compile(source);
+	$(".zoo_class_image").click(loadClassAnimal)
 
-		var content = animal_template(animals.class[parseInt($(this).attr("id"))]);
-		var class_index = parseInt($(this).attr("id"))
-		$("#content").html(content);
-		
-		//breadcrumb
-		$("#animal_class").text(animals.class[class_index].name);
-		
-		//after second level of template instantiated, js added 
-		$(".zoo_animal_image").click(function(){
-			var source = $("#individual_animal").html();
-			animal_template = Handlebars.compile(source);
-
-			species_index = parseInt($(this).attr("id"));
-			var content = animal_template(animals.class[class_index].animals[species_index]);
-			$("#content").html(content);
-		
-			//sub breadcrumb automatically built
-			$("#animal_class").text(animals.class[class_index].name);
-			$("#animal_class").attr("onclick", "loadClassAnimal(" + class_index + ")");
-			$("#animal_species").text(animals.class[class_index].animals[species_index].name);
-
-		})
-		
-	})
-	
-	
 }
 
 $(document).ready(loadZooAnimals);
