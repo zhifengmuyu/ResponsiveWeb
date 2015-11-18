768bbb49676bfa6f6ec0efd407796c6d7d900657
michael.liu <michael.liu@tlscontact.com>
2015-11-18 18:38:49 +0800

    Exam for algorithms

---
+++ algorithm/graph/digraph.js
@@ -0,0 +1,95 @@
+var digraph={
+  total_v:0,//total number of vertex
+  total_e:0,//total number of edge
+  adjlist:[],
+  
+  initialize: function(alist){
+  this.adjlist=alist;
+  console.log("alist:" + alist.length)
+  this.total_v=this.adjlist.length;
+  },
+
+  adj:function(i){//return adjcent list for vertex i
+	return this.adjlist[i];
+  },
+  
+  addEdge:function( v,  w){
+	var adj_v = this.adjlist[v];
+	this.adj_v[this.adj_v.length]=w;
+	this.total_e=this.total_e+1;
+  },
+  
+  reverse:function(){
+  }
+}
+
+//function DFS
+function dfs(dgraph,  index){
+	var adjarray=dgraph.adj(index);
+	console.log(index);
+	marked[index]=true;	
+	//console.log("l:" +adjarray.length);
+	for (var i = 0; i < adjarray.length; i++)
+	{
+		//console.log(adjarray[0]);
+		var adjvex = adjarray[i];
+		if(!marked[adjvex]){
+			edgeto[adjvex]=index;
+			dfs(dgraph, adjvex);
+		}
+		//console.log("never here:"+i);
+	}
+	reversepostorder.push(index);
+}
+
+//data
+var data=[
+[5,1],
+[],
+[0,3],
+[5,2],
+[3,2],
+[4],
+[9,4,8,0],
+[6,9],
+[6],
+[11,10],
+[12],
+[4,12],
+[9]
+];
+
+var datadiag=[
+[5,1,6],
+[],
+[0,3],
+[5],
+[],
+[4],
+[9,4],
+[6],
+[7],
+[11,10,12],
+[],
+[12],
+[]
+];
+
+console.log(datadiag.length);
+digraph.initialize(datadiag);
+//var marked = new array(digraph.total_v);
+var marked = [];
+var edgeto = [];
+var reversepostorder=[];
+//var edgeto = new array(digraph.total_v);
+console.log("start");
+for(var v=0; v<digraph.total_v; v ++)
+	if(!marked[v])
+		dfs(digraph, v);
+	
+console.log(edgeto);
+while(reversepostorder.length>0)
+	{ 
+	var data = reversepostorder.pop();
+	console.log(data);
+	}
