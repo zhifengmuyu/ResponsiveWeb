b1db0b1b1bc413c3a785a01620a400cb6ac42d5e
michael.liu <michael.liu@tlscontact.com>
2015-11-19 18:30:44 +0800

    add number to letters mapping

--- algorithm/graph/digraph.js
+++ algorithm/graph/digraph.js
@@ -1,3 +1,25 @@
+var lettertoNumber={
+A:0,
+B:1,
+C:2,
+D:3,
+E:4,
+F:5,
+G:6,
+H:7,
+}
+
+var numbertoLetter = [
+"A",
+"B",
+"C",
+"D",
+"E",
+"F",
+"G",
+"H"
+]
+
 var digraph={
   total_v:0,//total number of vertex
   total_e:0,//total number of edge
@@ -30,7 +52,7 @@ function bfs(dgraph,  start)
 	marked[start]= true;
 	while(queue.length>0){
 		var vex = queue.pop();
-		console.log("vex=" +vex); //log
+		console.log("vex=" +numbertoLetter[vex]); //log
 		var adjarray=dgraph.adj(vex);
 		for(var i = 0; i < adjarray.length; i++)
 		{
@@ -95,23 +117,46 @@ var datadiag=[
 [12],
 []
 ];
-
-console.log(datadiag.length);
-digraph.initialize(datadiag);
+ 
+ var exam1data=[
+ 	[5]         ,
+    [0, 6, 2 ]  ,
+    []          ,
+    [6, 2 ]     ,
+    [0 ]        ,
+    [1, 4 ]     ,
+    [7, 5, 2]   ,
+    [3]         
+ ]
+ 
+ var exam2data=[
+[lettertoNumber.E ]														,
+[lettertoNumber.C, lettertoNumber.A, lettertoNumber.F, lettertoNumber.E],
+[lettertoNumber.G, lettertoNumber.D                                   ] ,
+[lettertoNumber.H, lettertoNumber.G                                   ] ,
+[]                                                                      ,
+[lettertoNumber.G, lettertoNumber.E, lettertoNumber.C                 ] ,
+[]                                                                      ,
+[lettertoNumber.G                                                     ]
+ ]
+console.log(exam2data.length);
+digraph.initialize(exam2data);
 //var marked = new array(digraph.total_v);
 var marked = [];
 var edgeto = [];
 var reversepostorder=[];
 //var edgeto = new array(digraph.total_v);
 console.log("start");
-for(var v=0; v<digraph.total_v; v ++)
-	if(!marked[v])
-		dfs(digraph, v);
-		//bfs(digraph, 0);
-	
-console.log(edgeto);
-while(reversepostorder.length>0)
-{ 
- var data = reversepostorder.pop();
-console.log("schedule " + data);
- }
+
+//bfs(digraph, 0);
+
+ for(var v=0; v<digraph.total_v; v ++)
+	 if(!marked[v])
+		 dfs(digraph, v);
+
+ console.log(edgeto);
+ while(reversepostorder.length>0)
+ { 
+	var data = reversepostorder.pop();
+	console.log("schedule " + numbertoLetter[data]);
+  }
